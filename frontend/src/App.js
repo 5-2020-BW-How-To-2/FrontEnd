7f08fa6ea1a29ac44c06356c67add3ba7cb35718
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-05-26 11:32:54 -0500

    folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
     return (
         <div className='App'>
             <h1>Front End Hell nah</h1>
+            {/* <Login />
+            <SignUp /> */}
         </div>
     );
 }
