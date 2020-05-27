1b7ade482ff63c0e0cac13b49e77ff234d8cc081
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-05-26 20:10:46 -0500

    added styles to login

--- frontend/src/components/StickyNav.js
+++ frontend/src/components/StickyNav.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 
 const StickyNav = () => {
     return (
-        <div className="nav">
+        <div className='nav'>
             <div>
-            <h2 id="logo">LifeHacks</h2>
+                <h2 id='logo'>LifeHacks</h2>
             </div>
-            <div className="navbuttons">
-            <Link><button to="/home">Home</button></Link>
-            <Link to="/signup"><button>Sign Up</button></Link>
-            <Link to="/login"><button>Login</button></Link>
-            <Link to="/dashboard"><button>TEST FOR DASH</button></Link>
+            <div className='navbuttons'>
+                <Link>
+                    <button to='/home'>Home</button>
+                </Link>
+                <Link to='/signup'>
+                    <button>Sign Up</button>
+                </Link>
+                <Link to='/login'>
+                    <button>Login</button>
+                </Link>
+                <Link to='/dashboard'>
+                    <button>TEST FOR DASH</button>
+                </Link>
             </div>
-            
         </div>
-    )
-}
+    );
+};
 export default StickyNav;
